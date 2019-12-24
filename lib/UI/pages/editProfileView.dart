3469895d20ecabd7c3f0153feeadd9b98336615b
limiRatnayake <42302128+limiRatnayake@users.noreply.github.com>
2019-12-24 07:17:03 +0530

    blood collector

--- lib/UI/pages/editProfileView.dart
+++ lib/UI/pages/editProfileView.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class  EditProfileView extends StatefulWidget {
+class EditProfileView extends StatefulWidget {
   @override
   _EditProfileViewState createState() => _EditProfileViewState();
 }
@@ -9,19 +9,105 @@ class _EditProfileViewState extends State<EditProfileView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-       
-        actionsIconTheme: null,
-       
-      ),
-      body: Card(
-        child: Column(
-          children: <Widget>[
-
+        appBar: AppBar(
+          actionsIconTheme: null,
+          leading: Container(
+            child: Icon(Icons.check),
+          ),
+          title: Text("Edit Profile"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {},
+            ),
           ],
         ),
-      ),
-      
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 180.0,
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Center(
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("assets/person.jpg"),
+                          minRadius: 40,
+                          maxRadius: 60,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Center(
+                        child: InkWell(
+                          child: Text(
+                            "Change Profile Photo",
+                            style:
+                                TextStyle(color: Colors.blue, fontSize: 16.0),
+                          ),
+                          onTap: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Card(
+                      child: ListTile(
+                        title: Text("Name"),
+                        subtitle: TextField(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Card(
+                      child: ListTile(
+                        title: Text("Email Address"),
+                        subtitle: TextField(),
+                      ),
+                    ),
+                     SizedBox(
+                      height: 10.0,
+                    ),
+                    Card(
+                      child: ListTile(
+                        title: Text("Phone Number"),
+                        subtitle: TextField(),
+                      ),
+                    ),
+                     SizedBox(
+                      height: 10.0,
+                    ),
+                    Card(
+                      child: ListTile(
+                        title: Text("Gender"),
+                        subtitle: TextField(),
+                      ),
+                    ),
+                     SizedBox(
+                      height: 10.0,
+                    ),
+                    Card(
+                      child: ListTile(
+                        title: Text("Blood Group"),
+                        subtitle: TextField(),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/UI/pages/settingView.dart
+++ lib/UI/pages/settingView.dart
@@ -24,29 +24,95 @@ class _SettingViewState extends State<SettingView> {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: <Widget>[
-                Card(
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: AssetImage("assets/person.jpg"),
-                          radius: 30,
-                        ),
-                        contentPadding: EdgeInsets.only(left: 35.0, top: 15.0),
-                        title: Text("Tom Riddle \n+941578444"),
-                        subtitle: Text("Email"),
+                Container(
+                  height: 150.0,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 35.0, top: 10.0),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              CircleAvatar(
+                                backgroundImage:
+                                    AssetImage("assets/person.jpg"),
+                                minRadius: 30,
+                                maxRadius: 45,
+                              ),
+                              SizedBox(
+                                width: 25.0,
+                              ),
+                              Column(
+                                // mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("Azenz Allibe Dongro"),
+                                  SizedBox(
+                                    height: 8.0,
+                                  ),
+                                  Text("+94 165252326"),
+                                  SizedBox(
+                                    height: 8.0,
+                                  ),
+                                  Text("Email"),
+                                  SizedBox(
+                                    height: 10.0,
+                                  ),
+                                  
+                                ],
+                              ),
+                              
+                            ],
+                          ),
+                          InkWell(
+                                    child: Text(
+                                      "Edit",
+                                      style: TextStyle(color: Colors.blue, fontSize: 18.0),
+                                    ),
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EditProfileView()));
+                                    },
+                                  ),
+                        ],
                       ),
-                      ButtonTheme(
-                        child: FlatButton(
-                          child: Text("Edit"),
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=> EditProfileView()));
-                          },
-                        ),
-                      )
-                    ],
+                    ),
                   ),
                 ),
+                // Card(
+                //   child: Column(
+                //     children: <Widget>[
+
+                //       ListTile(
+                //         leading: Container(
+                //           child: CircleAvatar(
+                //             backgroundImage: AssetImage("assets/person.jpg"),
+                //             backgroundColor: Colors.black,
+                //             minRadius: 55,
+                //             maxRadius: 80,
+                //           ),
+                //         ),
+                //         // contentPadding: EdgeInsets.only(left: 35.0, top: 15.0),
+                //         title: Text("Tom Riddle \n+941578444"),
+                //         subtitle: Text("Email"),
+                //       ),
+                //       ButtonTheme(
+                //         child: FlatButton(
+                //           child: Text("Edit"),
+                //           onPressed: () {
+                //             Navigator.push(
+                //                 context,
+                //                 MaterialPageRoute(
+                //                     builder: (context) => EditProfileView()));
+                //           },
+                //         ),
+                //       )
+                //     ],
+                //   ),
+                // ),
                 Card(
                   child: Column(children: <Widget>[
                     ListTile(
