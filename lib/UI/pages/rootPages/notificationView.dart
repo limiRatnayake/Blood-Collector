6a5da59d4b221034b33fe7e3fd9d57e9aac56816
Achala Jeevaka <jeevakarathnayaka@gmail.com>
2020-10-04 01:31:14 +0530

    hj

--- lib/UI/pages/rootPages/notificationView.dart
+++ lib/UI/pages/rootPages/notificationView.dart
@@ -1,4 +1,5 @@
 import 'package:blood_collector/UI/widgets/appTopBar.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class NotificationView extends StatefulWidget {
@@ -7,6 +8,52 @@ class NotificationView extends StatefulWidget {
 }
 
 class _NotificationViewState extends State<NotificationView> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  List<Message> _messages;
+  _getToken() {
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+  }
+
+  _configureFirebaseListners() {
+    _firebaseMessaging.configure(
+      onMessage: (message) async {
+        print("onMessage: $message");
+        _setMessage(message);
+      },
+      onResume: (message) async {
+        print("onMessage: $message");
+        _setMessage(message);
+      },
+      onLaunch: (message) async {
+        print("onMessage: $message");
+        _setMessage(message);
+      },
+    );
+  }
+
+//get the firebase message
+  _setMessage(Map<String, dynamic> messages) {
+    final notification = messages['notification'];
+    final data = messages['data'];
+    final String titile = notification['title'];
+    final String body = notification['body'];
+    final String message = data['message'];
+    setState(() {
+      Message m = Message(titile, body, message);
+      _messages.add(m);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getToken();
+    _configureFirebaseListners();
+    _messages = List<Message>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,26 +62,31 @@ class _NotificationViewState extends State<NotificationView> {
           child: AppTopBar(
             title: "Notification",
           )),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.picture_in_picture),
-                title: Text("Name"),
-                subtitle: Text("Reason"),
-              ),
+      body: ListView.builder(
+        itemCount: null == _messages ? 0 : _messages.length,
+        itemBuilder: (context, index) {
+          print(_messages[index].message);
+          return Card(
+            child: ListTile(
+              leading: Icon(Icons.picture_in_picture),
+              title: Text(_messages[index].message),
+              // subtitle: Text(_messages[index].message),
             ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.picture_in_picture),
-                title: Text("Name"),
-                subtitle: Text("Reason"),
-              ),
-            )
-          ],
-        ),
+          );
+        },
       ),
     );
+  }
+}
+
+class Message {
+  String titile;
+  String body;
+  String message;
+
+  Message(titile, body, message) {
+    this.titile = titile;
+    this.body = body;
+    this.message = message;
   }
 }
